861d0132393de790ce533b835598de0301a11623
EXP <289065406@qq.com>
2019-11-02 18:04:56 +0800

    修正发布脚本

--- deploy-for-github.sh
+++ deploy-for-github.sh
@@ -1,11 +1,9 @@
 # Github Pages 要求所发布的 Html 路径不能有下划线，否则无法解析
 # 此脚本的目的是把 GitBook 生成的 _book 目录复制到 book
 
-Remove-Item gitbook/book -recurse
-
-docker run --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 exp/gitbook-server gitbook build
-
-Copy-Item gitbook/_book gitbook/book -recurse
+rm -rf gitbook/book
+# docker run --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 exp/gitbook-server gitbook build
+cp -r gitbook/_book gitbook/book
 
 echo "Deploy Finish."
 echo "You can push to github now."
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 exp/gitbook-server gitbook build
-
