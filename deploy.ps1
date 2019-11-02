3ed574343d9b46e25077ad925f00799409f610c3
EXP <289065406@qq.com>
2019-11-03 02:01:02 +0800

    更新插件

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,9 @@
 # Github Pages 要求所发布的 Html 路径不能有下划线，否则无法解析
 # 此脚本的目的是把 GitBook 生成的 _book 目录复制到 book
 
+Remove-Item gitbook/_book -recurse
 Remove-Item gitbook/book -recurse
-# docker run --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 exp/gitbook-server gitbook build
+docker run --rm -v "$PWD/gitbook:/gitbook" exp/gitbook-server gitbook build
 Copy-Item gitbook/_book gitbook/book -recurse
 
 echo "Deploy Finish."
