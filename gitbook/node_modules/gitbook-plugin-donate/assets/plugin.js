ed8d269d3be8a8315f706b763e98b36e58249122
EXP <289065406@qq.com>
2019-11-05 00:51:30 +0800

    修正打赏插件不能识别页面层次问题

--- gitbook/node_modules/gitbook-plugin-donate/assets/plugin.js
+++ gitbook/node_modules/gitbook-plugin-donate/assets/plugin.js
@@ -6,47 +6,67 @@ require(['gitbook', 'jQuery'], function(gitbook, $) {
 	var wechatText;
 	var alipayText;
 
+
+	// console.log("gitbook object: ",gitbook)
+	// console.log(gitbook);
+	
+
 	function insertDonateLink() {
-    if ($('.gitbook-donate').length === 0 && wechatURL !== undefined && (wechatURL !== '' || alipayURL !== '')) {
+		if ($('.gitbook-donate').length === 0 && wechatURL !== undefined && (wechatURL !== '' || alipayURL !== '')) {
+			var pageDepth = gitbook.state.page.depth + 1;
 			var html = [
-        '<div class="gitbook-donate">',
-        '<div>' + titleText + '</div>',
-        '<button id="rewardButton" disable="enable" onclick="var qr = document.getElementById(\'QR\'); if (qr.style.display === \'none\') {qr.style.display=\'block\';} else {qr.style.display=\'none\'}">',
-        '<span>' + buttonText + '</span>',
-        '</button>',
-        '<div id="QR" style="display: none;">'
-      ];
-			if (wechatURL !== '') {
+				'<div class="gitbook-donate">',
+				'<div>' + titleText + '</div>',
+				'<button id="rewardButton" disable="enable" onclick="var qr = document.getElementById(\'QR\'); if (qr.style.display === \'none\') {qr.style.display=\'block\';} else {qr.style.display=\'none\'}">',
+				'<span>' + buttonText + '</span>',
+				'</button>',
+				'<div id="QR" style="display: none;">'
+			];
+
+			var _wechatURL = wechatURL;
+			if (_wechatURL !== '') {
+				for(pd = 0; pd < pageDepth; pd++) {
+					_wechatURL = "../" + _wechatURL;
+				}
+
 				html = html.concat([
-          '<div id="wechat" style="display: inline-block">',
-					'<a href="' + wechatURL + '" class="fancybox" rel="group">',
-          '<img id="wechat_qr" src="' + wechatURL + '" alt="WeChat Pay"/>',
+					'<div id="wechat" style="display: inline-block">',
+					'<a href="' + _wechatURL + '" class="fancybox" rel="group">',
+					'<img id="wechat_qr" src="' + _wechatURL + '" alt="WeChat Pay"/>',
 					'</a>',
-          '<p>' + wechatText + '</p>',
-          '</div>'
-        ]);
+					'<p>' + wechatText + '</p>',
+					'</div>'
+				]);
 			}
-			if (alipayURL !== '') {
+
+			var _alipayURL = alipayURL;
+			if (_alipayURL !== '') {
+				for(pd = 0; pd < pageDepth; pd++) {
+					_alipayURL = "../" + _alipayURL;
+				}
+
 				html = html.concat([
-          '<div id="alipay" style="display: inline-block">',
-					'<a href="' + alipayURL+ '" class="fancybox" rel="group">',
-          '<img id="alipay_qr" src="' + alipayURL + '" alt="Alipay"/>',
+					'<div id="alipay" style="display: inline-block">',
+					'<a href="' + _alipayURL+ '" class="fancybox" rel="group">',
+					'<img id="alipay_qr" src="' + _alipayURL + '" alt="Alipay"/>',
 					'</a>',
-          '<p>' + alipayText + '</p>', '</div>'
-        ]);
+					'<p>' + alipayText + '</p>', '</div>'
+				]);
 			}
+
 			html = html.concat(['</div>', '</div>']);
 			$('.page-inner section.normal:last').after(html.join(''));
 		}
 	}
 
 	gitbook.events.bind('start', function(e, config) {
-    wechatURL = config.donate.wechat || '';
+		titleText = config.donate.title || '';
+		buttonText = config.donate.button || '赏';
+		wechatURL = config.donate.wechat || '';
 		wechatText = config.donate.wechatText || '微信捐赠';
 		alipayURL = config.donate.alipay || '';
 		alipayText = config.donate.alipayText || '支付宝捐赠';
-		titleText = config.donate.title || '';
-		buttonText = config.donate.button || '赏';
+		
 		insertDonateLink();
 	});
 
