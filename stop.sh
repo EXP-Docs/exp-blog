0639d5102af25eaeb737e7b28722529e35e118db
lyy289065406 <289065406@qq.com>
2020-08-16 16:07:07 +0800

    add rss

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 DOCKER_ID=`docker ps -aq --filter name=exp-blog`
-if [ ! -z "${DOCKER_ID}" ]; then
-    docker stop ${DOCKER_ID}
-    # docker rm ${DOCKER_ID}
+if [ ! -z "$DOCKER_ID" ]; then
+    docker stop $DOCKER_ID
+    # docker rm $DOCKER_ID
 fi
