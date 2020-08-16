c9d049954f2330719f22dfab69aa4b554b4ed2ee
EXP <289065406@qq.com>
2020-08-16 19:20:18 +0800

    add rss

--- stop.ps1
+++ stop.ps1
@@ -1,5 +1,5 @@
 $DOCKER_ID = (docker ps -aq --filter name=exp-blog)
-if([String]::IsNullOrEmpty($DOCKER_ID)) {
+if(![String]::IsNullOrEmpty($DOCKER_ID)) {
     docker stop $DOCKER_ID
     # docker rm $DOCKER_ID
 }
