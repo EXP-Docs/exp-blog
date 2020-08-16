e1f81a7c383165e79a4edfd07b37085aae900eaa
lyy289065406 <289065406@qq.com>
2020-08-16 15:12:23 +0800

    add rss

--- start.ps1
+++ start.ps1
@@ -1,1 +1,1 @@
-docker run -d --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 expm02/gitbook-server gitbook serve
+docker run -d --rm -v "$PWD/gitbook:/gitbook" -p 4000:4000 --name="exp-blog" expm02/gitbook-server gitbook serve
