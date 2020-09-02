706795d54681f5c360e1da5c7cf9e33b25107f99
EXP <289065406@qq.com>
2020-09-02 18:42:04 +0800

    #!/bin/sh

--- stop.sh
+++ stop.sh
@@ -1,4 +1,6 @@
-DOCKER_ID=`docker ps -aq --filter name=exp-blog`
+#!/bin/sh
+
+DOCKER_ID=`docker ps -aq --filter name=hexo-blog`
 if [ ! -z "$DOCKER_ID" ]; then
     docker stop $DOCKER_ID
     # docker rm $DOCKER_ID
